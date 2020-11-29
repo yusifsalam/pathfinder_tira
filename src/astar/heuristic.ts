32731b0d1edfbf551a3a8b5ff775ea4b459ed533
Yusif Salam-zade <yusalam@gmail.com>
2020-11-29 22:32:38 +0200

    implement octile heuristic function

--- src/astar/heuristic.ts
+++ src/astar/heuristic.ts
@@ -1,16 +1,26 @@
+import { absoluteDiff, minVal } from '../utils/math'
+
 /**
  * Manhattan distance heuristic function
  * @param n1 Starting node
  * @param n2 Destination node
  */
 export const heuristicManhattan = (n1: IPoint, n2: IPoint): number => {
-  const xDiff =
-    n1.positionX >= n2.positionX
-      ? n1.positionX - n2.positionX
-      : n2.positionX - n1.positionX
-  const yDiff =
-    n1.positionY >= n2.positionY
-      ? n1.positionY - n2.positionY
-      : n2.positionY - n1.positionY
+  const xDiff = absoluteDiff(n1.positionX, n2.positionX)
+  const yDiff = absoluteDiff(n1.positionY, n2.positionY)
   return xDiff + yDiff
+}
+
+/**
+ * Octile distance heuristic function
+ * @param n1 Starting node
+ * @param n2 Destination node
+ */
+export const heuristicOctile = (n1: IPoint, n2: IPoint): number => {
+  const D1 = 1
+  const D2 = Math.sqrt(2)
+  const xDiff = absoluteDiff(n1.positionX, n2.positionX)
+  const yDiff = absoluteDiff(n1.positionY, n2.positionY)
+  const minDiff = minVal(xDiff, yDiff)
+  return D1 * (xDiff + yDiff) + (D2 - 2 * D1) * minDiff
 }
---
+++ src/utils/math.ts
@@ -0,0 +1,18 @@
+/**
+ * Calculates an absolute value of the the difference between two numbers
+ * @param a First argument
+ * @param b Second argument
+ */
+export const absoluteDiff = (a: number, b: number) => {
+  const diff = a >= b ? a - b : b - a
+  return diff
+}
+
+/**
+ * Calculates the smaller number from the two arguments
+ * @param a First argument
+ * @param b Second argument
+ */
+export const minVal = (a: number, b: number) => {
+  return a > b ? a : b
+}
