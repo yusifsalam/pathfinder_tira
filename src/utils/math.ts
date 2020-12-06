5ff1ff6d2ed25f40fe4692a59046ab35f2c47bfb
Yusif Salam-zade <yusalam@gmail.com>
2020-12-06 22:23:09 +0200

    implement method for getting node with best f value

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,9 +1,11 @@
+import { Node } from '../astar/node'
+
 /**
  * Calculates an absolute value of the the difference between two numbers
  * @param a First argument
  * @param b Second argument
  */
-export const absoluteDiff = (a: number, b: number) => {
+export const absoluteDiff = (a: number, b: number): number => {
   const diff = a >= b ? a - b : b - a
   return diff
 }
@@ -13,6 +15,23 @@ export const absoluteDiff = (a: number, b: number) => {
  * @param a First argument
  * @param b Second argument
  */
-export const minVal = (a: number, b: number) => {
+export const minVal = (a: number, b: number): number => {
   return a > b ? a : b
+}
+
+/**
+ * Returns the Node with the lowest f-score
+ * @param arr Array containing Nodes
+ */
+export const lowestFScore = (arr: Node[]): Node => {
+  let lowestScore: number = Infinity
+  let bestNode: Node
+  for (let n of arr) {
+    if (n.fValue < lowestScore) {
+      lowestScore = n.fValue
+      bestNode = n
+    }
+  }
+  if (!bestNode) return
+  return bestNode
 }
